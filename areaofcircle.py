cf0fe9ea95b08049ccbca95a4f0a2458536d5c7b
studyfree20 <studyfree20@gmail.com>
2020-01-30 00:04:56 -0800

    Area of Circle

---
+++ areaofcircle.py
@@ -0,0 +1,13 @@
+"""
+requirements
+radius
+pie
+
+"""
+#Code starts here BSCIT-01-0345/2018
+radius=int(input("Enter radius    "))
+area=3.142*radius*radius
+print("Area is   ")
+print(area)
+
+#Code starts here BSCIT-01-0345/2018
